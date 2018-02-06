9e810e48d3063746964a0b11f4a5490cb17fb44f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-05 18:14:00 -0700

    Updates job create test to include categories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,8 +5,9 @@ class JobsController < ApplicationController
   end
 
   def new
+    @categories = Category.all
     @company = Company.find(params[:company_id])
-    @job = Job.new()
+    @job = @company.jobs.new
   end
 
   def create
@@ -15,8 +16,6 @@ class JobsController < ApplicationController
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
       redirect_to company_job_path(@company, @job)
-    else
-      render :new
     end
   end
 
@@ -47,6 +46,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id)
   end
 end
