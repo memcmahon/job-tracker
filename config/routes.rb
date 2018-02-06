2f21f6da2d00517d15613ef1fb795d21f5cfc94a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-06 15:47:42 -0700

    Upates routes to only those being used

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :jobs, only: [:show] do
-    resources :comments
+    resources :comments, only: [:index, :new, :create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
