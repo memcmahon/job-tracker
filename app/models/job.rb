ae8964ddd6b88f256bba3c62f047d95896ef08e9
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-06 18:20:22 -0700

    Adds dashboard level of interest count

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,8 @@ class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
   has_many :comments
+
+  def self.count_by_interest
+    group(:level_of_interest).order(level_of_interest: :desc).count
+  end
 end
--- spec/features/dashboard/user_can_see_dashboard_spec.rb
+++ spec/features/dashboard/user_can_see_dashboard_spec.rb
@@ -6,15 +6,14 @@ describe "user sees application dashboard" do
       company = Company.create!(name: "ESPN")
       category = Category.create!(title: "Entertainment")
       job_1 = company.jobs.create!(title: "Developer", description: "So fun!", level_of_interest: 80, city: "Denver", category_id: category.id)
-      job_1 = company.jobs.create!(title: "Junior Developer", description: "So fun!", level_of_interest: 90, city: "Denver", category_id: category.id)
-      job_1 = company.jobs.create!(title: "Anouncer", description: "So fun!", level_of_interest: 80, city: "Denver", category_id: category.id)
-      job_1 = company.jobs.create!(title: "Janitor", description: "So fun!", level_of_interest: 90, city: "Denver", category_id: category.id)
+      job_2 = company.jobs.create!(title: "Junior Developer", description: "So fun!", level_of_interest: 90, city: "Denver", category_id: category.id)
+      job_3 = company.jobs.create!(title: "Anouncer", description: "So fun!", level_of_interest: 80, city: "Denver", category_id: category.id)
 
       visit dashboard_path
 
-      expect(page).to have_content("Jobs by Interest Level:")
-      expect(page).to have_content("80: Developer, Anouncer")
-      expect(page).to have_content("90: Junior Developer, Janitor")
+      expect(page).to have_content("Count of Jobs by Interest Level")
+      expect(page).to have_content("80: 2")
+      expect(page).to have_content("90: 1")
     end
   end
 end
