9bda646fbdbca7344a0231b7001a0e6f3cf6efcb
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-07 18:14:21 -0700

    Refactors contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,16 @@
 class ContactsController < ApplicationController
   def create
-    @company = Company.find(params[:company_id])
+    call_company
     @contact = @company.contacts.new(contact_params)
     @contact.save
     redirect_to company_path(@company)
   end
 
   private
+
+  def call_company
+    @company = Company.find(params[:company_id])
+  end
 
   def contact_params
     params.require(:contact).permit(:full_name, :position, :email)
